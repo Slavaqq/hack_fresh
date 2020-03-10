af99269f2a9a66b62cc558cfd233aad4b12a6297
Slava <slavaqq@gmail.com>
2020-03-10 17:13:05 +0100

    create goods dictionary

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import requests
+import bs4
+
+URL = "http://freshpoint.freshserver.cz/frontend/web/index.php?r=device%2Fview&id=60"
 
 def main():
-    r = requests.get("http://freshpoint.freshserver.cz/frontend/web/index.php?r=device%2Fview&id=60")
-    print(r.text)
+    r = requests.get(URL)
+    # print(r.content)
+    soup = bs4.BeautifulSoup(r.content, "html.parser")
+    tables = soup.find_all("tbody")
+    for table in tables:
+        goods = {}
+        for row in table.find_all("tr"):
+            good = row.find_all("td")[0].text
+            quantity = row.find_all("td")[1].text
+            goods[good] = quantity
+
+    print(goods)
 
 main()
