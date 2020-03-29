fcfb458be79a1b8e3df27efd7debd0a2e67c2ec3
Slava <slavaqq@gmail.com>
2020-03-29 18:22:58 +0200

    refactoring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import json
-from typing import Optional
+from typing import Any, Dict
 
 import requests
 import bs4
@@ -8,18 +8,17 @@ URL = "http://freshpoint.freshserver.cz/frontend/web/index.php?r=device%2Fview&i
 ITEMS_JSON = "items.json"
 
 
-def get_content(url: str) -> Optional[str]:
+def get_content(url: str) -> bytes:
     response = requests.get(url)
-    if response.status_code != 200:
-        return None
+    response.raise_for_status()
     return response.content
 
 
-def cook_soup(content: str) -> bs4.BeautifulSoup:
+def cook_soup(content: bytes) -> bs4.BeautifulSoup:
     return bs4.BeautifulSoup(content, "html.parser")
 
 
-def get_items(soup: bs4.BeautifulSoup) -> dict:
+def get_items(soup: bs4.BeautifulSoup) -> Dict[str, int]:
     items_table = soup.find_all("tbody")[-1]
     return {get_item(tr): get_quantity(tr) for tr in items_table.find_all("tr")}
 
@@ -31,66 +30,50 @@ def is_on_sale(soup: bs4.BeautifulSoup) -> bool:
     return False
 
 
-def get_sale_items(soup: bs4.BeautifulSoup) -> dict:
-    items_table = soup.find("tbody")
-    return {get_item(tr): get_sale(tr) for tr in items_table.find_all("tr")}
+def get_sale_items(soup: bs4.BeautifulSoup) -> Dict[str, str]:
+    if is_on_sale(soup):
+        items_table = soup.find("tbody")
+        return {get_item(tr): get_sale(tr) for tr in items_table.find_all("tr")}
+    return {}
 
 
-def get_quantity(tr) -> int:
+def get_quantity(tr: bs4.element.Tag) -> int:
     return int(tr.find_all("td")[1].text)
 
 
-def get_item(tr) -> str:    
+def get_item(tr: bs4.element.Tag) -> str:
     return tr.find_all("td")[0].text
 
 
-def get_sale(tr) -> str:
+def get_sale(tr: bs4.element.Tag) -> str:
     return tr.find_all("td")[2].text
 
 
-def save_json(obj, file_path):
+def save_json(obj: Any, file_path: str) -> None:
     with open(file_path, "w") as f:
-        json.dump(obj, f)
+        json.dump(obj, f, indent=4)
 
 
-def open_json(file_path):
+def open_json(file_path: str) -> Any:
     with open(file_path, "r") as f:
         return json.load(f)
 
 
-def items_diff(current: dict, previous: dict):
-    current_set = set(current)
-    previous_set = set(previous)
-    intersection = current_set & previous_set
-    diff = {}
-    # 1. je v nabídce a počet se zvýšil
-    diff["increase"] = {item: current[item] - previous[item] for item in intersection if current[item] > previous[item]} 
-    # 2. je v nabídce a počet se snížil
-    diff["decrease"] = {item: previous[item] - current[item] for item in intersection if current[item] < previous[item]}
-    # 3. byl v nabídce a už není, vyprodano
-    diff["out_of_stock"] = previous_set - current_set
-    # 4. nebyl v nabídce a už je, naskladněno
-    diff["new_in_stock"] = current_set - previous_set
-    return diff
+def items_diff(current: Dict[str, int], previous: Dict[str, int]) -> Dict[str, int]:
+    items_union = set(current) | set(previous)
+    return {item: current.get(item, 0) - previous.get(item, 0) for item in items_union}
 
 
-def changes_and_sales(soup: bs4.BeautifulSoup) -> dict:
-    current_items = get_items(soup)
-    previous_items = open_json(ITEMS_JSON)
-    save_json(current_items, ITEMS_JSON)
-    result = {"changes": {}, "sales": {}}
-    result["changes"] = items_diff(current_items, previous_items)
-    if is_on_sale(soup):
-        result["sales"] = get_sale_items(soup)
-    return result
-
-
-def main():
+def main() -> None:
     content = get_content(URL)
-    if content is not None:
-        soup = cook_soup(content)
-        x = changes_and_sales(soup)
-        print(x)
+    soup = cook_soup(content)
+    previous_items = open_json(ITEMS_JSON)
+    current_items = get_items(soup)
+    save_json(current_items, ITEMS_JSON)
+    sales_items = get_sale_items(soup)
+    print(sales_items)
+    diff_items = items_diff(current_items, previous_items)
+    print(diff_items)
 
 
 if __name__ == "__main__":
